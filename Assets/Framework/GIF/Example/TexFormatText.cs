98863f9bf616079ffb9234c3785220fdefb24c29
dongyh <dongyh@yomob.com>
2022-03-14 19:37:04 +0800

    GIF 1.提交测试场景

---
+++ Assets/Framework/GIF/Example/TexFormatText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TexFormatText : MonoBehaviour
+{
+    public Text texMemory;
+
+
+    public void Update()
+    {
+        texMemory.text = string.Format("Tex Memory = " + (Texture.currentTextureMemory / 1024).ToString("0.0") + "KB");
+    }
+}
